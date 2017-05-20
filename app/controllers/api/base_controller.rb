2d6aa3827a5595e2e1aba7c4b52b40535837f9ac
Nina Louw <ninalouw@mac.com>
2017-05-20 11:31:03 -0700

    auth

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::BaseController < ApplicationController
-  before_action :authenticate
+  # before_action :authenticate
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
